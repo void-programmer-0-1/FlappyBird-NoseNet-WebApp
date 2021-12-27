57cbfd3f1fe47f93e9a6015e7f9617aa7558abc9
santoshm <santoshm@digikriya.com>
2021-12-27 19:15:53 +0530

    new route image is added

--- WEBAPP/dj/flappybird/game/views.py
+++ WEBAPP/dj/flappybird/game/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+import numpy as np
+import re
+import base64
+from io import BytesIO
+from PIL import Image
 
 def home(request):
     
@@ -19,5 +21,24 @@ def hand_game(request):
 def nose_game(request):
     
     return render(request,"nose_game.html",None)
+
+
+def image(request):
+    
+    if request.method == "POST": 
+
+        image_b64 = request.values['image']
+        image_data = re.sub('^data:image/.+;base64,','',image_b64) 
+        decoded = base64.b64decode(image_data)
+
+        image_PIL = Image.open(BytesIO(decoded))
+        image_PIL.save("image.png")
+        
+        image_np = np.array(image_PIL,dtype=np.float32)
+        print(image_np[None,...].shape)
+
+        return "got image"
+    
+    return "waiting for image"
 
 
